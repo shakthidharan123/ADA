2f5f6aaccf49b56508d96f7c4bdb2f37961a3d38
shakthidharan123 <shakthidharan123@gmail.com>
2025-02-12 23:16:09 +0530

    Initial Commit

---
+++ app/(tabs)/Accident.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Accident = () => {
+  return (
+    <View>
+      <Text>Accident</Text>
+    </View>
+  )
+}
+
+export default Accident
