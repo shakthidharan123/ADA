3e4acf8e57065041addf5e5daf64b29aefa9adcf
adithiyanluffy <141423033+adithiyanluffy@users.noreply.github.com>
2025-03-12 23:16:18 +0530

    serverchanges

--- server/Schema.js
+++ server/Schema.js
@@ -1,7 +1,33 @@
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: String, // 'admin' or 'user'
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'user'], required: true },
+  location: { type: String }, // User's location
+  active_status: {
+    type: Boolean,
+    default: false // Default value is false (inactive)
+  },
+  accidents: [
+    {
+      accident: { type: mongoose.Schema.Types.ObjectId, ref: 'Accident' },
+      status: { type: String, enum: ['ongoing', 'completed'], required: true }
+    }
+  ]
 });
-module.exports = mongoose.model('User', userSchema);
+
+const accidentSchema = new mongoose.Schema({
+  location: { type: String, required: true },
+  complete_address: { type: String, required: true },
+  lat: { type: Number, required: true },
+  lng: { type: Number, required: true },
+  maps_link: { type: String },
+  datetime: { type: Date, required: true },
+  severity_label: { type: String, enum: ['accident', 'minor', 'major'], required: true }
+});
+
+module.exports = {
+  User: mongoose.model('User', userSchema),
+  Accident: mongoose.model('Accident', accidentSchema),
+};
